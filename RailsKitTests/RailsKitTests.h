139db0007d061a38349411a56a5debad4ad51a58
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-10-17 17:03:21 +0100

    Basic Cocoa-based framework project added

---
+++ RailsKitTests/RailsKitTests.h
@@ -0,0 +1,13 @@
+//
+//  RailsKitTests.h
+//  RailsKitTests
+//
+//  Created by Roy Ratcliffe on 17/10/2011.
+//  Copyright (c) 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RailsKitTests : SenTestCase
+
+@end
---
+++ RailsKitTests/RailsKitTests.m
@@ -0,0 +1,32 @@
+//
+//  RailsKitTests.m
+//  RailsKitTests
+//
+//  Created by Roy Ratcliffe on 17/10/2011.
+//  Copyright (c) 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import "RailsKitTests.h"
+
+@implementation RailsKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RailsKitTests");
+}
+
+@end
